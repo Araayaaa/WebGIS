f5c695ebf62614387666c16d363fe2a5df5055a2
Araya's Project <119692205+Araayaaa@users.noreply.github.com>
2026-06-14 09:29:59 +0700

    fix: add JSON headers to get_data.php and OPTIONS support

--- sig-03/get_data.php
+++ sig-03/get_data.php
@@ -1,5 +1,15 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 require_once 'auth.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 requireLogin();
 
